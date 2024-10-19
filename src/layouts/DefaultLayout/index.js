f0844261ab994872122ccbab70bd939c4e2e6920
tunahan24 <anhsilver2404@gmail.com>
2024-10-19 17:20:03 +0700

    update DefaultLayout forder

--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames/bind';
 import Header from '../components/Header';
-import Sidebar from './Sidebar';
+import Sidebar from '~/layouts/components/Sidebar';
 import styles from './DefaultLayout.module.scss';
 
 const cx = classNames.bind(styles);

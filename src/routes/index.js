0b59ef24309c0f5ea7387e55aad0c2f6498a519d
tunahan24 <anhsilver2404@gmail.com>
2024-10-19 17:18:51 +0700

    Update routes forder

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,21 @@
 import HeaderOnly from '~/layouts/HeaderOnly';
 
+import config from '~/config';
+
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
 import Search from '~/pages/Search';
+import Live from '~/pages/Live';
 
 const publicRoutes = [
-    { path: '/', component: Home },
-    { path: '/following', component: Following },
-    { path: '/@:nickname', component: Profile },
-    { path: '/upload', component: Upload, layout: HeaderOnly },
-    { path: '/search', component: Search, layout: null },
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.live, component: Live },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.search, component: Search, layout: null },
 ];
 
 const privateRoutes = [];
